5b95c3698b72d32b7d54c1a328b0970606e927f3
Sprayingjosh <joshuadamilare3@gmail.com>
2024-05-10 10:56:50 +0100

    Initial commit

---
+++ question1.js
@@ -0,0 +1,11 @@
+function isEven (num) {
+    return num % 2 ===0;
+}
+let value = 16;
+if (isEven(value))
+    console.log('{value}is even');
+else
+{
+    console.log ( '{value} is odd');
+}
+
---
+++ question3.js
@@ -0,0 +1,12 @@
+function checkStringLength(inputString){
+const lenght = inputString.lenght;
+    if (lenght % 2 === 0){
+        console.log ="even";
+    }
+    else {
+        console.log= "odd"}
+    const userInput = prompt ("coming")
+    console.log = checkStringLength("coming")
+}
+    
+    
